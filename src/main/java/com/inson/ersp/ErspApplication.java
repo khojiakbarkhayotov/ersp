9bc55dffae566210d7bcff51589d64d17639c09f
kh.khayotov <khayotovkhojiakbar@gmail.com>
2024-07-03 13:08:42 +0500

    initial commit

---
+++ src/main/java/com/inson/ersp/ErspApplication.java
@@ -0,0 +1,13 @@
+package com.inson.ersp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inson/ersp/ErspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inson.ersp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
